325f6367054f3d18fe5f6d7343be0076b2b92ff8
Eric Nordebäck <eric.nordebaeck@rocket-internet.de>
2017-10-09 16:33:52 +0200

    Added grunt, webpack, basic infrastructure

---
+++ src/content-scripts/main.js
@@ -0,0 +1,7 @@
+import $ from 'jquery';
+
+function init() {
+  console.log('Extension loaded!');
+}
+
+$(document).ready(init);
---
+++ webpack.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  entry: './src/content-scripts/main.js',
+  output: {
+    filename: './build/tmp/content-scripts/cs-bundle.js'
+  }
+};
