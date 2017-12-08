aa0ab115aefa36f453a79ce98a736f5b31c4d362
Sergiu Tomsa <sergiu.tomsa@shopwings.com>
2017-12-08 10:32:04 +0100

    Add sha1 as list of recognize micalgs

--- src/constants/smimeSpecificationConstants.js
+++ src/constants/smimeSpecificationConstants.js
@@ -2,7 +2,7 @@ const smimeSpecificationConstants = {
   signatureNodeContentTypeValues: ["application/x-pkcs7-signature", "application/pkcs7-signature"],
   rootNodeContentTypeValue: "multipart/signed",
   rootNodeContentTypeProtocol: "application/pkcs7-signature",
-  rootNodeContentTypeMessageIntegrityCheckAlgorithms: ["md5", "sha-1", "sha-224", "sha-256", "sha-384", "sha-512", "unknown"],
+  rootNodeContentTypeMessageIntegrityCheckAlgorithms: ["md5", "sha1", "sha-1", "sha-224", "sha-256", "sha-384", "sha-512", "unknown"],
 };
 
 export default smimeSpecificationConstants;
