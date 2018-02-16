783e2d104badb1e54bc30b1fb987f84c16b797a4
Sergiu Tomsa <sergiu.tomsa@rocket-internet.de>
2018-02-16 13:56:02 +0100

    Clean Tooltipster logs (#53)

--- src/modules/tooltipService.js
+++ src/modules/tooltipService.js
@@ -10,12 +10,18 @@ class TooltipService {
     if (result.code === smimeVerificationResultCodes.VERIFICATION_OK) {
       messageTheme = this.config.themeSuccess;
     }
+    let tooltipMultiple = false;
 
     $('.inboxsdk__message_attachment_icon').attr('title', result.message);
+    if ($(this.config.selector).hasClass('tooltipstered')) {
+      tooltipMultiple = true;
+    }
+
     this.tooltip = $(this.config.selector).tooltipster({
       animation: this.config.animation,
       theme: messageTheme,
-      trigger: this.config.trigger
+      trigger: this.config.trigger,
+      multiple: tooltipMultiple
     });
   }
 }
